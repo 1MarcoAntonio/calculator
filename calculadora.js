84a5ac298ff5ed0cdbcb541108bec10c3f739590
Aluno <aluno@naverecife.local>
2022-09-21 21:36:38 -0300

    inicial

---
+++ calculadora.js
@@ -0,0 +1,30 @@
+const div1 = document.createElement("div")
+const div2 = document.createElement("div")
+const resultado = document.createElement("output")
+const imput1 = document.createElement("input")
+const imput2 = document.createElement("input")
+const buttonSoma = document.createElement("button")
+const buttonMult = document.createElement("button")
+const buttonVezes = document.createElement("button")
+const buttonDiv = document.createElement("button")
+imput1.style.padding = "5px 20px"
+imput2.style.padding = "5px 20px"
+buttonSoma.style.padding = "20px 40px"
+buttonMult.style.padding = "20px 40px"
+buttonVezes.style.padding = "20px 40px"
+buttonDiv.style.padding = "20px 40px"
+buttonSoma.addEventListener("click",function(){
+    
+})
+div1.appendChild(imput1)
+div1.appendChild(imput2)
+div2.appendChild(buttonSoma)
+div2.appendChild(buttonMult)
+div2.appendChild(buttonVezes)
+div2.appendChild(buttonDiv)
+
+
+document.body.appendChild(div1)
+document.body.appendChild(div2)
+
+
